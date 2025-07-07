d5b799a5c7e14acfac9d46b2166a1b1ba38efcf2
Alina Zhdanova <gdanova98@gmail.com>
2025-07-08 01:47:04 +0700

    Step 2 - Create a WEB application.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,6 +21,10 @@ repositories {
 }
 
 dependencies {
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("org.slf4j:slf4j-simple:2.0.7")
+    implementation("io.javalin:javalin-rendering:6.1.3")
+    implementation("gg.jte:jte:3.1.9")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,18 @@
 package hexlet.code;
 
+import io.javalin.Javalin;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        var app = getApp();
+        app.start(7070);
+    }
+
+    public static Javalin getApp() {
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+        });
+        app.get("/", ctx -> ctx.result("Hello, World!"));
+        return app;
     }
 }
