9ace77b247b4079bd550267eeeb03fc267907991
Alina Zhdanova <gdanova98@gmail.com>
2025-07-08 02:10:20 +0700

    Step 2 - Create a WEB application.

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,9 +3,14 @@ package hexlet.code;
 import io.javalin.Javalin;
 
 public class App {
+
     public static void main(String[] args) {
         var app = getApp();
         app.start(7070);
+    }
+
+    private static String getDatabaseUrl() {
+        return System.getenv().getOrDefault("DATABASE_URL", "jdbc:h2:mem:project");
     }
 
     public static Javalin getApp() {
