0364f617adbad3afa53f4b61b1797d20122eecd6
Alina Zhdanova <gdanova98@gmail.com>
2025-07-07 17:29:51 +0700

    Step 1 - Initialize the project.

---
+++ app/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    application
+    checkstyle
+    id("org.sonarqube") version "6.2.0.5505"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "Alina-Zhdanova_java-project-722")
+        property("sonar.organization", "alina-zhdanova")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
