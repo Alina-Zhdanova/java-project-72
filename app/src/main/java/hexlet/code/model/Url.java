bd93fba0ae3b25d6d8e0ac9f68dc9e9ac30a547d
Alina Zhdanova <gdanova98@gmail.com>
2025-07-09 23:12:18 +0700

    Step 3 - JDBC. 1. HikariCP is connected to the project. 2. A dependency has been added for working with H2 type databases and HikariCP has been configured. 3. The Url model has been created. 4. A base class for repositories has been created. 5. The initial database structure has been created. 6. The database connection is configured so that the in–memory H2 database is used in local development, and PostgreSQL is used in the production environment.

--- app/src/main/java/hexlet/code/model/Url.java
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -2,4 +2,4 @@ package hexlet.code.model;
 
 import java.sql.Timestamp;
 
-public record Url (int id, String name, Timestamp createdAt) { }
+public record Url(int id, String name, Timestamp createdAt) { }
